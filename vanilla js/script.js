c4c23494e5f47230a8162e457843bfe6aca6b3e0
ortori <ortori@gmail.com>
2022-11-14 13:35:37 +0200

    Added vanilla JavaScript solution

---
+++ vanilla js/script.js
@@ -0,0 +1,47 @@
+const colors = [
+  "#A0E4CB",
+  "#5DA7DB",
+  "#7DE5ED",
+  "#F8C4B4",
+  "#7978FF",
+  "#749F82",
+  "#98A8F8",
+  "#D58BDD",
+];
+
+const displayData = (data, nested, color, div) => {
+  const mainDiv = document.createElement("div");
+  const pId = document.createElement("p");
+  const pSiteName = document.createElement("p");
+  const pSiteUrl = document.createElement("p");
+  const aSiteUrl = document.createElement("a");
+  mainDiv.classList.add("main-container");
+  mainDiv.style.minWidth = nested ? "200px" : "300px";
+  mainDiv.style.marginLeft = nested ? "auto" : "";
+  mainDiv.style.backgroundColor = colors[color];
+  pId.textContent = `Id: ${data.id}`;
+  pSiteName.textContent = `Site Name: ${data.name}`;
+  pSiteUrl.textContent = `Site Url: ${data.name}`;
+  aSiteUrl.href = `http://${data.url}`;
+  aSiteUrl.target = "_blank";
+  aSiteUrl.rel = "noreferrer";
+  aSiteUrl.appendChild(pSiteUrl);
+  mainDiv.appendChild(pId);
+  mainDiv.appendChild(pSiteName);
+  mainDiv.appendChild(aSiteUrl);
+  if (nested) {
+    div.appendChild(mainDiv);
+  } else {
+    const body = document.querySelector("body");
+    body.appendChild(mainDiv);
+  }
+  data?.subData?.forEach((data) => {
+    displayData(data, true, color + 1, mainDiv);
+  });
+};
+
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((data) => displayData(data, false, 0));
+  });
